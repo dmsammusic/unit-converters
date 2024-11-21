5fbf2acce742b4e0db61fc54bcc9804499f38eb4
Sam M <dmsammmusic@gmail.com>
2024-11-21 16:19:49 +0530

    bugfix

--- dist/main.d.ts
+++
@@ -1,7 +0,0 @@
-export { convertLength } from './converters/length/length-converter';
-export { convertVolume } from './converters/volume/volume-converter';
-export { convertArea } from './converters/area/area-converter';
-export { convertTemperature, TempConverter } from './converters/temp/temperature-converter';
-export { convertMass, convertWeight, roundMass, MassConverter } from './converters/mass/mass-converter';
-export { convertTime, roundTime, TimeConverter } from './converters/time/time-converter';
-export { convertSpeed, roundSpeed, SpeedConverter } from './converters/speed/speed-converter';
